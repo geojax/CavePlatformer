ac5b9c181336c49e7a6589b1e5466aee6d5d9426
geojax <50026101+geojax@users.noreply.github.com>
2020-11-26 08:59:38 -0800

    platform layer and trying to raycast
    Can someone help with raycasting? It should return true when the player is near the ground

--- HWGDCavePlatformer/Assets/Scripts/PlayerController.cs
+++ HWGDCavePlatformer/Assets/Scripts/PlayerController.cs
@@ -6,26 +6,44 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb2d;
     [SerializeField] float speed = 5;
+
     [SerializeField] float jumpForce = 5;
+    bool canJump = true;
+    Ray jumpRay;
+
+    LayerMask platformLayer;
     
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        platformLayer = LayerMask.GetMask("Platform");
     }
 
     // Update is called once per frame
     void Update()
     {
         rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb2d.velocity.y);
-        if (Input.GetKeyDown("w") || Input.GetKeyDown("up"))
+        if ((Input.GetKeyDown("w") || Input.GetKeyDown("up")) && canJump)
         {
             Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        jumpRay.origin = transform.position; // What's wrong with this?
+        jumpRay.direction = Vector3.down;
+        if (Physics.Raycast(jumpRay, 10f, platformLayer))
+        {
+            canJump = true;
+            Debug.Log("I can jump!");
         }
     }
 
     void Jump()
     {
         rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        canJump = false;
     }
 }
